f6668d420479405e9a2115ed4db78f21492060af
Krutoy-sourse <anisimalekseenkov@gmail.com>
2026-04-10 21:14:40 +0800

    Refactor bot initialization and remove proxy setup
    Removed proxy configuration and used environment variable for token.

--- main.py
+++ main.py
@@ -1,21 +1,11 @@
 from aiogram import types, Dispatcher, Bot
 from aiogram.utils import executor
 from random import randint
-from credits import token
+import os
 
-from aiohttp_socks import ProxyConnector
-from aiohttp import ClientSession
+TOKEN = os.getenv("8266023377:AAHr4maCljTr1CXp5HE2LTIN2j7rXKdnmNU")
 
-# 🔐 ВСТАВЬ СЮДА СВОЙ ПРОКСИ
-PROXY_URL = "socks5://5.130.4.14:1081"
-# пример:
-# PROXY_URL = "socks5://user:pass@123.123.123.123:1080"
-
-# создаём подключение через прокси
-connector = ProxyConnector.from_url(PROXY_URL)
-session = ClientSession(connector=connector)
-
-bot = Bot(token=token, session=session)
+bot = Bot(TOKEN)
 dp = Dispatcher(bot)
 
 
@@ -30,6 +20,4 @@ async def credits(message: types.Message):
 
 
 if __name__ == "__main__":
-    executor.start_polling(dp, skip_updates=True)
-
-    
+    executor.start_polling(dp, skip_updates=True)  
